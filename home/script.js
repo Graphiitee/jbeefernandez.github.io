44ac1f7ca610ded437df4486ebf4cd4388a17f15
JBeeFernandez <johnbenedict.fernandez08@gmail.com>
2026-05-02 12:30:24 +0400

    Update on Home Page
    Focusing on the navigation bar as of the moment

---
+++ home/script.js
@@ -0,0 +1,9 @@
+//  Navigation bar background change when it scrolls
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
